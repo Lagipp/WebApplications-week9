8a763a9742387ccefa7c33290b7aa310aaa9b0eb
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-11-18 13:40:36 +0200

    task 4 done?

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,16 +1,41 @@
 import MyList from "./MyList";
-import useState from 'react';
+import { useState } from 'react';
 
 const MyContainer = () => {
+
+    const [items, setItems] = useState([
+        { id: "1", text: "list item 1" },
+        { id: "2", text: "list item 2" },
+    ]);
+
+    const [textAreaText, setTextAreaText] = useState("");
+
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        
+        addListItem({text: textAreaText})
+
+        setTextAreaText("")
+    }
+
+    const addListItem = (item) => {
+        const id = Math.floor(Math.random() * 102931 + 12301 - 33);
+        const newListItem = {id, ...item}
+        setItems([...items, newListItem])
+    }
+
     return (
         <div>
             <MyList 
-                header="this is a header"
-                items={[
-                    { id: "1", text: "list item 1" },
-                    { id: "2", text: "list item 2" },
-                ]}
+                header="This is a header"
+                items={items}
             />
+            <form onSubmit={onSubmit}>
+            <textarea placeholder="Add text here" onChange={(event) => setTextAreaText(event.target.value)} value={textAreaText}></textarea>
+            <br />
+            <input className="btn" type="submit" value="Add text"></input>
+            </form>
         </div>
     )
 }
